1a1900d9bbff54535269e7829c06a807d019e5b3
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-10-20 23:25:35 +0300

    num

--- js/index.js
+++ js/index.js
@@ -3,15 +3,3 @@ document.querySelector(".header__toggle").addEventListener("click", () => {
     .querySelector(".header__nav")
     .classList.toggle("header__nav--active");
 });
-// Добавляем обработчик клавиш
-document.addEventListener("keydown", (event) => {
-  const nav = document.querySelector(".header__nav");
-  if (event.key === "Enter") {
-    nav.classList.toggle("header__nav--active");
-  } else if (
-    event.key === "Escape" &&
-    nav.classList.contains("header__nav--active")
-  ) {
-    nav.classList.remove("header__nav--active");
-  }
-});
