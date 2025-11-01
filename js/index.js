aafcb460f19b3aa0127deb80a1e32e8c9f0a7a18
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-01 23:14:15 +0200

    8

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ document.querySelector(".header__toggle").addEventListener("click", () => {
 document.querySelector(".header__hed").addEventListener("click", () => {
   document.querySelector(".header__hed").classList.toggle("header__hed");
 });
+
+document.querySelector(".header__toggle").addEventListener("click", () => {
+  document
+    .querySelector(".header__nav")
+    .classList.toggle("header__nav--active");
+});
+
+document.querySelector(".header__hed").addEventListener("click", () => {
+  document.querySelector(".header__hed").classList.toggle("header__hed");
+});
